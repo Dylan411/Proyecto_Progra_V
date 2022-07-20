3109dbb0d4ab9832bf8d08b7a6739d3b61a1971e
Dylan411 <57643953+Dylan411@users.noreply.github.com>
2022-07-19 18:37:14 -0600

    new interface in the main new individual interface

---
+++ app/src/main/java/com/example/proyecto_progra5/InformationFragment.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto_progra5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InformationFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_place_information)
+    }
+
+}
